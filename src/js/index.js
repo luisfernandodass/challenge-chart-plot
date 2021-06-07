6ce43c050f165fbe9981b2886961b804cc70c034
Luis Fernando <luisfernandodass@gmail.com>
2021-06-07 00:18:41 -0300

    it is getting the timestamp, os, browser

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,54 @@
 const buttonGenerateChart = document.getElementById('buttonGenerateChart');
 
-/*function time() {
-  let today = new Date();
-  let beginTime = today.getTime();
+let now = new Date();
+let timestamp = now.getTime();
 
-  console.log(beginTime);
-}*/ 
 
+let os = navigator.userAgent.slice(13).split(';')[0];
+
+let browser = navigator.appCodeName;
+
+console.log(os);
+console.log(browser);
 
 
 function generateChart() {
   const console = document.getElementById('console');
 
-const requestURL = 'src/js/data.json';
-const request = new XMLHttpRequest();
+  const requestURL = 'src/js/data.json';
+  const request = new XMLHttpRequest();
 
-request.open('GET', requestURL);
+  request.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
+  request.responseType = 'json';
+  request.send();
 
-request.onload = function() {
-  const superHeroes = request.response;
-  
-  showHeroes(superHeroes);
-}
+  request.onload = function () {
+    const loadConsole = request.response;
 
-function showHeroes(jsonObj) {
-  var heroes = jsonObj['events'];
-
-  for (var i = 0; i < heroes.length; i++) {
-    var myArticle = document.createElement('article');
-    var p1 = document.createElement('p');
- 
-    p1.textContent = 
-    ' type: ' + heroes[i].type + ',' +
-    ' timestamp: ' + heroes[i].timestamp + ',' +
-    ' os: ' + heroes[i].os + ',' +
-    ' browser: ' + heroes[i].browser + ',' +
-    ' min_response_time: ' + heroes[i].min_response_time + ',' +
-    ' max_response_time: ' + heroes[i].max_response_time;
- 
-    myArticle.appendChild(p1);
-  
-    console.appendChild(myArticle);
+    consoleInputData(loadConsole);
   }
-}
 
+  function consoleInputData(jsonObj) {
+    const heroes = jsonObj['events'];
 
+    for (var i = 0; i < heroes.length; i++) {
+      const myArticle = document.createElement('article');
+      const p1 = document.createElement('p');
+
+      p1.textContent =
+        ' type: ' + heroes[i].type + ',' +
+        ' timestamp: ' + timestamp + ',' +
+        ' os: ' + os + ',' +
+        ' browser: ' + browser + ',' +
+        ' min_response_time: ' + heroes[i].min_response_time + ',' +
+        ' max_response_time: ' + heroes[i].max_response_time;
+
+      myArticle.appendChild(p1);
+
+      console.appendChild(myArticle);
+    }
+  }
 
   const context = document.getElementById('myChart').getContext('2d');
   const myChart = new Chart(context, {
@@ -104,7 +105,7 @@ function showHeroes(jsonObj) {
         {
           label: 'Linux Firefox Max Response Time',
           data: [76, 88],
-          
+
           backgroundColor: "#104E8B",
           borderColor: "#104E8B",
           fill: false,
@@ -152,10 +153,10 @@ function showHeroes(jsonObj) {
             usePointStyle: true,
             color: [
               "black",
-                     
+
             ],
             textAlign: 'left',
-            padding: 12      
+            padding: 12
           }
         }
       },
@@ -164,4 +165,13 @@ function showHeroes(jsonObj) {
 }
 
 buttonGenerateChart.addEventListener('click', generateChart);
-// buttonGenerateChart.addEventListener('click', time);
+
+/*
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+document.write('Your OS: '+OSName);
+*/
