52cce77a127ad99b96f84ae785687aad47cb7369
Luis Fernando <luisfernandodass@gmail.com>
2021-06-07 19:49:04 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,27 @@
-const buttonGenerateChart = document.getElementById('buttonGenerateChart');
-
-let now = new Date();
-let timestamp = now.getTime();
-
+// Below we have a way to get the operation system from user
 let os;
 if (navigator.appVersion.indexOf("Win")!=-1) os = "windows";
 if (navigator.appVersion.indexOf("Mac")!=-1) os = "mac";
 if (navigator.appVersion.indexOf("X11")!=-1) os = "linux";
 
-// let os = navigator.userAgent.slice(13).split(';')[0];
-
+// Below we have a way to get the browser from user
 let browser = navigator.appCodeName;
 
-function maxResponseTime(funcao) {
- 
-  const fromJson = jsonObj['events'];
-  for(let i = 0; i <fromJson.lenght; i++){
-    var b = now.getSeconds();
+let now = new Date();
+let timestamp = now.getTime();
 
-    return performance.now() - i;
-  }
+function maxResponseTime() { 
+    var b = now.getSeconds() + 10;
+    return b.toString();  
 }
 
-function minResponseTime(funcao) {
-
-  const fromJson = jsonObj['events'];
-  for(let i = 0; i <fromJson.lenght; i--){
+function minResponseTime() {
     var b = now.getSeconds();
-
-    return performance.now() - i;
-  }
+    return b.toString(); 
 }
 
+// This function generate the chart and the data in console (main function in our app)
 function generateChart() {
-  const console = document.getElementById('console');
 
   const requestURL = 'src/js/data.json';
   const request = new XMLHttpRequest();
@@ -50,38 +38,55 @@ function generateChart() {
     consoleInputData(loadConsole);
   }
 
+  // Below we put the data in console
   function consoleInputData(jsonObj) {
     const fromJson = jsonObj['events'];
 
-    for (var i = 0; i < fromJson.length; i++) {
-      const myArticle = document.createElement('article');
-      const clientInformation = document.createElement('p');
+    const console = document.getElementById('console');
+    const section = document.createElement('section');
+    const typeSpan = document.createElement('p');
+    const typeData = document.createElement('p');
+    const typeStop = document.createElement('p');
 
-      clientInformation.textContent =
-        ' type: ' + fromJson[i].type + ',' +
+    typeSpan.textContent =
+    ' type: ' + fromJson[1].type + ',' + // we're getting the type 'span' from Json
+    ' timestamp: ' + timestamp + ',' +
+    ' begin: ' + timestamp + ',' +
+    ' end:' + 1819862460000;
+
+    for (var i = 0; i < fromJson.length; i++) {
+     
+      typeData.textContent = 
+        ' type: ' + fromJson[2].type + ',' + // we're getting the type 'data' from Json
         ' timestamp: ' + timestamp + ',' +
         ' os: ' + os + ',' +
         ' browser: ' + browser + ',' +
-        ' min_response_time: ' + maxResponseTime() + i--
-        ' max_response_time: ' + minResponseTime() + i;
+        ' min_response_time: ' + minResponseTime() + 
+        ' max_response_time: ' + maxResponseTime();
+    }  
+    
+    typeStop.textContent =
+    ' type: ' + fromJson[10].type + ',' + // we're getting the type 'stop' from Json
+    ' timestamp: ' + timestamp ;
 
-      myArticle.appendChild(clientInformation);
-
-      console.appendChild(myArticle);
-    }
+    section.appendChild(typeSpan);  
+    section.appendChild(typeData);
+    section.appendChild(typeStop);
+  
+    console.appendChild(section); 
   }
 
-  // Below we have the chart
+  // Below we have the chart (the only thing that I don't need to refactor)
   const context = document.getElementById('myChart').getContext('2d');
   const myChart = new Chart(context, {
     type: 'line',
     data: {
 
-      labels: ['00:00', '00:01'],
+      labels: [ minResponseTime(),  maxResponseTime()],
       datasets: [
         {
           label: 'Linux Chrome Min Response Time',
-          data: [2, maxResponseTime()],
+          data: [2, 10],
           backgroundColor: "#7CCD7C",
           borderColor: "#7CCD7C",
           fill: false,
@@ -90,7 +95,7 @@ function generateChart() {
         },
         {
           label: 'Linux Chrome Max Response Time',
-          data: [minResponseTime(), maxResponseTime()],
+          data: [48, 75],
           backgroundColor: "#008B45",
           borderColor: "#008B45",
           fill: false,
@@ -99,7 +104,7 @@ function generateChart() {
         },
         {
           label: 'Mac Chrome Min Response Time ',
-          data: [minResponseTime()), maxResponseTime()],
+          data: [20, 8],
           backgroundColor: "#8968CD",
           borderColor: "#8968CD",
           fill: false,
@@ -108,7 +113,7 @@ function generateChart() {
         },
         {
           label: 'Mac Chrome Max Response Time ',
-          data: [74, maxResponseTime()],
+          data: [74, 65],
           backgroundColor: "#551A8B",
           borderColor: "#551A8B",
           fill: false,
@@ -117,7 +122,7 @@ function generateChart() {
         },
         {
           label: 'Linux Firefox Min Response Time',
-          data: [2, maxResponseTime()],
+          data: [2, 12],
           backgroundColor: "#87CEFA",
           borderColor: "#87CEFA",
           fill: false,
@@ -126,7 +131,7 @@ function generateChart() {
         },
         {
           label: 'Linux Firefox Max Response Time',
-          data: [76, maxResponseTime()],
+          data: [76, 50],
           backgroundColor: "#104E8B",
           borderColor: "#104E8B",
           fill: false,
@@ -135,7 +140,7 @@ function generateChart() {
         },
         {
           label: 'Mac Firefox Min Response Time',
-          data: [18, maxResponseTime()],
+          data: [18, 30],
           backgroundColor: "#FFD700",
           borderColor: "#FFD700",
           fill: false,
@@ -144,7 +149,7 @@ function generateChart() {
         },
         {
           label: 'Mac Firefox Max Response Time',
-          data: [74, maxResponseTime()],
+          data: [74, 80],
           backgroundColor: "#FF7F00",
           borderColor: "#FF7F00",
           fill: false,
@@ -182,4 +187,5 @@ function generateChart() {
   });
 }
 
+const buttonGenerateChart = document.getElementById('buttonGenerateChart');
 buttonGenerateChart.addEventListener('click', generateChart);
