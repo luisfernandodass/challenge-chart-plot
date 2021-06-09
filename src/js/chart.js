0c8078832f99dfe691bfa7633552bc507338f7ac
Luis Fernando <luisfernandodass@gmail.com>
2021-06-09 14:41:38 -0300

    up

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,72 +1,35 @@
-// This function generate the chart and the data in console (main function in our app)
-function generateChart() {
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
 
-  const requestURL = '/src/js/data.json'; // This is getting the json
-  const request = new XMLHttpRequest();
+require(['vs/editor/editor.main'], function() {
 
-  request.open('GET', requestURL);
+    // JSON object we want to edit
+    let jsonCode = 
+       
+    {type: 'span', timestamp: 0, begin: 0, end: 0}
+       
+    const modelUri = monaco.Uri.parse("json://grid/settings.json");
+    const jsonModel = monaco.editor.createModel(JSON.stringify(jsonCode), "json", modelUri);
 
-  request.responseType = 'json';
-  request.send();
+    const editor = monaco.editor.create(document.getElementById('console'), {
+      model: jsonModel,  
+      theme: "vs-dark",       
+      readOnly: false,
+     
+      automaticLayout: true,
+      minimap: {
+        enabled: false
+      }
+    });
 
-  // Below the response loads after request loads first, it's more efficient.
-  request.onload = function () {
-    const loadConsole = request.response;
+   
+  // const codeFromUser = editor.getValue();
+  // let code = JSON.parse(jsonCode);
+  function generateChart() {
+    
+    var b = editor.getValue();
+    b = jsonCode.begin;
+    console.log(b);
 
-    consoleInputData(loadConsole);
-  }
-
-  // Below we put the data in console
-  function consoleInputData(jsonObj) {
-    const fromJson = jsonObj['events'];
-
-    const console = document.getElementById('console');
-    const section = document.createElement('section');
-    const typeStart = document.createElement('p');
-    const typeSpam = document.createElement('p');
-    const typeStop = document.createElement('p');
-
-    typeStart.textContent =
-      ' type: ' + fromJson[0].type +  ',' +
-      ' timestamp: ' + fromJson[0].timestamp +  ',' +
-      ' select: ' + fromJson[0].select +  ',' +
-      ' group: ' + fromJson[0].group;
-
-    typeSpam.textContent =
-      ' type: ' + fromJson[1].type +  ',' +
-      ' timestamp:  ' + fromJson[1].timestamp +  ',' +
-      ' begin: ' + fromJson[1].begin +  ',' +
-      ' end: ' + fromJson[1].end;
-
-    section.appendChild(typeStart);
-    section.appendChild(typeSpam);
-
-    // here we're getting only the type 'data' from Json
-    for (var i = 2; i < fromJson.length - 1; i++) { // "we're using -1" for don't get the "type:stop"
-
-      const typeData = document.createElement('p');
-
-      typeData.textContent =
-        ' type: ' + fromJson[i].type + ',' + 
-        ' timestamp: ' + fromJson[i].timestamp + ',' +
-        ' os: ' + fromJson[i].os + ',' +
-        ' browser: ' + fromJson[i].browser + ',' +
-        ' min_response_time: ' + fromJson[i].min_response_time + ',' +
-        ' max_response_time: ' + fromJson[i].max_response_time;
-
-      section.appendChild(typeData);
-    }
-
-    typeStop.textContent =
-      ' type: ' + fromJson[10].type + ',' +
-      ' timestamp: ' + fromJson[10].timestamp;
-
-    section.appendChild(typeStop);
-
-    console.appendChild(section);
-  }
-
-  // Below we have the chart (the only thing that I don't need to refactor)
   const context = document.getElementById('myChart').getContext('2d');
   const myChart = new Chart(context, {
     type: 'line',
@@ -76,7 +39,7 @@ function generateChart() {
       datasets: [
         {
           label: 'Linux Chrome Min Response Time',
-          data: [0, 1],
+          data: [b, jsonCode.end],
           backgroundColor: "#7CCD7C",
           borderColor: "#7CCD7C",
           fill: false,
@@ -85,7 +48,7 @@ function generateChart() {
         },
         {
           label: 'Linux Chrome Max Response Time',
-          data: [1, 3],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#008B45",
           borderColor: "#008B45",
           fill: false,
@@ -94,7 +57,7 @@ function generateChart() {
         },
         {
           label: 'Mac Chrome Min Response Time ',
-          data: [0, 2],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#8968CD",
           borderColor: "#8968CD",
           fill: false,
@@ -103,7 +66,7 @@ function generateChart() {
         },
         {
           label: 'Mac Chrome Max Response Time ',
-          data: [1, 2],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#551A8B",
           borderColor: "#551A8B",
           fill: false,
@@ -112,7 +75,7 @@ function generateChart() {
         },
         {
           label: 'Linux Firefox Min Response Time',
-          data: [0, 3],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#87CEFA",
           borderColor: "#87CEFA",
           fill: false,
@@ -121,7 +84,7 @@ function generateChart() {
         },
         {
           label: 'Linux Firefox Max Response Time',
-          data: [1, 4],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#104E8B",
           borderColor: "#104E8B",
           fill: false,
@@ -130,7 +93,7 @@ function generateChart() {
         },
         {
           label: 'Mac Firefox Min Response Time',
-          data: [0, 3],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#FFD700",
           borderColor: "#FFD700",
           fill: false,
@@ -139,7 +102,7 @@ function generateChart() {
         },
         {
           label: 'Mac Firefox Max Response Time',
-          data: [1, 2],
+          data: [jsonCode.begin, jsonCode.end],
           backgroundColor: "#FF7F00",
           borderColor: "#FF7F00",
           fill: false,
@@ -171,7 +134,16 @@ function generateChart() {
       },
     }
   });
+  
+
+  
+ 
+  var b = editor.getValue();
+  
+  console.log(b)
 }
 
 const buttonGenerateChart = document.getElementById('buttonGenerateChart');
 buttonGenerateChart.addEventListener('click', generateChart);
+
+});
